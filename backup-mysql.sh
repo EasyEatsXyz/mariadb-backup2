921b5a78cf3ea815b23b87cd99b7c0b49b2be684
Aleksandr Soloshenko <capcom@soft-c.ru>
2021-07-17 22:18:50 +0700

    Added: files with common variables and functions

--- backup-mysql.sh
+++ backup-mysql.sh
@@ -2,33 +2,31 @@
 
 export LC_ALL=C
 
-days_of_backups=3
-backup_owner="backup"
-parent_dir="/var/backup/mysql"
-defaults_file="/var/backup/.my.cnf"
-todays_dir="${parent_dir}/$(date +%Y-%m-%d)"
+source "$(dirname "$0")/config.inc"
+source "$(dirname "$0")/common.inc"
+
 log_file="${todays_dir}/backup-progress.log"
-encryption_key_file="${parent_dir}/encryption_key"
-use_compression=1
-now="$(date +%m-%d-%Y_%H-%M-%S)"
-processors="$(nproc --all)"
-space_treshold_kb=52428800
 
-# Use this to echo to standard error
-error () {
-    printf "%s: %s\n" "$(basename "${BASH_SOURCE}")" "${1}" >&2
-    exit 1
+parse_args () {
+    for i in "$@"
+    do
+        case $i in
+            -f|--force)
+            force=1
+            ;;
+            *)
+                # unknown option
+                # echo $i
+            ;;
+        esac
+    done
 }
-
-trap 'error "An unexpected error occurred."' ERR
 
 sanity_check () {
     # Check user running the script
-    if [ "$(id --user --name)" != "$backup_owner" ]; then
-        error "Script can only be run as the \"$backup_owner\" user"
-    fi
+    check_user
 
-    if [ -f "${todays_dir}/.lock" ]; then
+    if [ ! ${force} ] && [ -f "${todays_dir}/.lock" ]; then
         error "Another process working"
     fi
 
@@ -46,6 +44,16 @@ set_options () {
         "--stream=xbstream"
         "--parallel=${processors}"
     )
+
+    if [ $(mysql --defaults-file=${defaults_file} -e "SHOW SLAVE STATUS\G" | wc -l) -ne 0 ]; then
+        mariabackup_args+=( "--slave-info" )
+    fi
+
+    openssl_args=(
+        "-aes-256-cbc"
+        "-kfile"
+        "${encryption_key_file}"
+    )
     
     backup_type="full"
 
@@ -54,7 +62,7 @@ set_options () {
     if grep -q -s "to_lsn" "${todays_dir}/xtrabackup_checkpoints"; then
         backup_type="incremental"
         lsn=$(awk '/to_lsn/ {print $3;}' "${todays_dir}/xtrabackup_checkpoints")
-        xtrabackup_args+=( "--incremental-lsn=${lsn}" )
+        mariabackup_args+=( "--incremental-lsn=${lsn}" )
     fi
 }
 
@@ -73,25 +81,23 @@ take_backup () {
     base_name="${todays_dir}/${backup_type}-${now}.xbstream"
     if [ -r ${encryption_key_file} ] && [ ${use_compression} -gt 0 ]; then
         full_name="${base_name}.gz.enc"
-        mariabackup "${mariabackup_args[@]}" "--target-dir=${todays_dir}" 2> "${log_file}" | gzip | openssl enc -aes-256-cbc -kfile ${encryption_key_file} > "${full_name}.incomplete"
+        mariabackup "${mariabackup_args[@]}" "--target-dir=${todays_dir}" 2> "${log_file}" | gzip | openssl enc "${openssl_args[@]}" > "${full_name}.incomplete"
     elif [ -r ${encryption_key_file} ]; then
         full_name="${base_name}.enc"
-        mariabackup "${mariabackup_args[@]}" "--target-dir=${todays_dir}" 2> "${log_file}" | openssl enc -aes-256-cbc -kfile ${encryption_key_file} > "${full_name}.incomplete"
+        mariabackup "${mariabackup_args[@]}" "--target-dir=${todays_dir}" 2> "${log_file}" | openssl enc "${openssl_args[@]}" > "${full_name}.incomplete"
     elif [ ${use_compression} -gt 0 ]; then
         full_name="${base_name}.gz"
         mariabackup "${mariabackup_args[@]}" "--target-dir=${todays_dir}" 2> "${log_file}" | gzip > "${full_name}.incomplete"
     else
         full_name="${base_name}"
         mariabackup "${mariabackup_args[@]}" "--target-dir=${todays_dir}" 2> "${log_file}" > "${full_name}.incomplete"
-    fi
-    
-    mv "${full_name}.incomplete" "${full_name}"
+    fi && mv "${full_name}.incomplete" "${full_name}"
 
     rm "${todays_dir}/.lock"
 }
 
 main () {
-    cd && sanity_check && set_options && rotate_old && take_backup
+    cd && parse_args $@ && sanity_check && set_options && rotate_old && take_backup
 
     # Check success and print message
     if tail -1 "${log_file}" | grep -q "completed OK"; then
@@ -102,4 +108,4 @@ main () {
     fi
 }
 
-main
+main $@
--- extract-mysql.sh
+++ extract-mysql.sh
@@ -2,40 +2,27 @@
 
 export LC_ALL=C
 
-backup_owner="backup"
-encryption_key_file="/backups/mysql/encryption_key"
+source "$(dirname "$0")/config.inc"
+source "$(dirname "$0")/common.inc"
+
 log_file="extract-progress.log"
-number_of_args="${#}"
-processors="$(nproc --all)"
-
-# Use this to echo to standard error
-error () {
-    printf "%s: %s\n" "$(basename "${BASH_SOURCE}")" "${1}" >&2
-    exit 1
-}
-
-trap 'error "An unexpected error occurred.  Try checking the \"${log_file}\" file for more information."' ERR
 
 sanity_check () {
     # Check user running the script
-    if [ "${USER}" != "${backup_owner}" ]; then
-        error "Script can only be run as the \"${backup_owner}\" user"
-    fi
-    
-    # Check whether the qpress binary is installed
-    if ! command -v qpress >/dev/null 2>&1; then
-        error "Could not find the \"qpress\" command.  Please install it and try again."
-    fi
+    check_user
     
     # Check whether any arguments were passed
     if [ "${number_of_args}" -lt 1 ]; then
         error "Script requires at least one \".xbstream\" file as an argument."
     fi
-    
-    # Check whether the encryption key file is available
-    if [ ! -r "${encryption_key_file}" ]; then
-        error "Cannot read encryption key at ${encryption_key_file}"
-    fi
+}
+
+set_options () {
+    openssl_args=(
+        "-aes-256-cbc"
+        "-kfile"
+        "${encryption_key_file}"
+    )
 }
 
 do_extraction () {
@@ -47,34 +34,36 @@ do_extraction () {
     
         # Extract the directory structure from the backup file
         mkdir --verbose -p "${restore_dir}"
-        xbstream -x -C "${restore_dir}" < "${file}"
 
-        xtrabackup_args=(
-            "--parallel=${processors}"
-            "--decrypt=AES256"
-            "--encrypt-key-file=${encryption_key_file}"
-            "--decompress"
-        )
-
-        xtrabackup "${xtrabackup_args[@]}" --target-dir="${restore_dir}"
-        find "${restore_dir}" -name "*.xbcrypt" -exec rm {} \;
-        find "${restore_dir}" -name "*.qp" -exec rm {} \;
+        if [ -r ${encryption_key_file} ] && [ ${use_compression} -gt 0 ]; then
+            openssl enc -d "${openssl_args[@]}" -in "${file}" | gzip -d | mbstream -x -C "${restore_dir}"
+        elif [ -r ${encryption_key_file} ]; then
+            openssl enc -d "${openssl_args[@]}" -in "${file}" | mbstream -x -C "${restore_dir}"
+        elif [ ${use_compression} -gt 0 ]; then
+            gunzip -c "${file}" | mbstream -x -C "${restore_dir}"
+        else
+            mbstream -x -C "${restore_dir}"
+        fi
     
         printf "\n\nFinished work on %s\n\n" "${file}"
     
-    done > "${log_file}" 2>&1
+    done 2> "${log_file}"
 }
 
-sanity_check && do_extraction "$@"
+main () {
+    set_options && sanity_check && do_extraction "$@"
 
-ok_count="$(grep -c 'completed OK' "${log_file}")"
+    ok_count="$(grep -c 'completed OK' "${log_file}")"
 
-# Check the number of reported completions.  For each file, there is an
-# informational "completed OK".  If the processing was successful, an
-# additional "completed OK" is printed. Together, this means there should be 2
-# notices per backup file if the process was successful.
-if (( $ok_count !=  $# )); then
-    error "It looks like something went wrong. Please check the \"${log_file}\" file for additional information"
-else
-    printf "Extraction complete! Backup directories have been extracted to the \"restore\" directory.\n"
-fi
+    # Check the number of reported completions.  For each file, there is an
+    # informational "completed OK".  If the processing was successful, an
+    # additional "completed OK" is printed. Together, this means there should be 2
+    # notices per backup file if the process was successful.
+    if (( $ok_count != $# )); then
+        error "It looks like something went wrong. Please check the \"${log_file}\" file for additional information"
+    else
+        printf "Extraction complete! Backup directories have been extracted to the \"restore\" directory.\n"
+    fi
+}
+
+main $@
